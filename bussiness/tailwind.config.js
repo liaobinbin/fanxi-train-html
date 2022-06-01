13b5b0f535a082da3914128ea0386b0ef6bf2ca3
Boiao Ch <doubiliao@gmail.com>
2022-06-01 22:10:16 +0800

    feat: main title and card

--- bussiness/tailwind.config.js
+++ bussiness/tailwind.config.js
@@ -1,9 +1,14 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-		      "./src/**/*.{html,js,ts,jsx,tsx,pug}",
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx,pug}"],
   theme: {
+    colors: {
+      primary: "#ff350d",
+      secondary: "#252429",
+      ...colors,
+    },
     extend: {},
   },
   plugins: [],
-}
+};
