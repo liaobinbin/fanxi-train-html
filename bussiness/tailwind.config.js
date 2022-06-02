147647cbd12d1e8120aeed27f3f29394b687c719
Boiao Ch <doubiliao@gmail.com>
2022-06-02 16:16:46 +0800

    feat: add bg-info teacher service component

--- bussiness/tailwind.config.js
+++ bussiness/tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx,pug}"],
   theme: {
     colors: {
+      ...colors,
       primary: "#ff350d",
       secondary: "#252429",
-      ...colors,
     },
     extend: {},
   },
